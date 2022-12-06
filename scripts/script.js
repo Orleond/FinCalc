a03fda0d410ffe863c41ef21c8cdc4e8177996c9
Mr. Orleond <dev.mr.orleond@gmail.com>
2022-12-06 16:27:15 +0300

    Create project. Lesson 1

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+
+alert('Сообщение с любым текстом');
+console.log('Сообщение с любым текстом');
