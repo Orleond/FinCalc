1d6fe78df283631c4528e9b5ae1d250495701451
Mr. Orleond <dev.mr.orleond@gmail.com>
2022-12-06 17:22:14 +0300

    Присваивание переменным значений. Вывод в консоль некоторой информации. Добавление и вывод budgetDay

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,23 @@
 'use strict';
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 1400,                                       // Доход за месяц
+    income = 'Фриланс',                                 // Дополнительный доход
+    addExpenses = 'интернет, коммуналка, продукты',     // Дополнительные расходы
+    deposit = false,                                    // Депозит в банке
+    mission = 5000,                                     // Цель
+    period = 12;                                        // Время на выполнение цели
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев. Цель заработать ' + mission + ' рублей.');
+
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log(addExpenses);
+
+let budgetDay = Math.floor(money / 30);                             // Дневной бюджет
+console.log(budgetDay);
 
 alert('Сообщение с любым текстом');
 console.log('Сообщение с любым текстом');
